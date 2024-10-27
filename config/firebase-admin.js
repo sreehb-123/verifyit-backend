81aea3226742d2eaa774b9ddd7ef8bc5b91b7d81
sreehb-123 <152023996+sreehb-123@users.noreply.github.com>
2024-10-27 13:31:32 +0530

    Update firebase-admin.js

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,5 +1,7 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../serviceAccount.json' assert { type: 'json' }; // Adjust the path accordingly
+import dotenv from 'dotenv';
+dotenv.config();
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
